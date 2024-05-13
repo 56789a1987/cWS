036c5a5457c4f32b3236f2e2dbbb8591eb96ac65
56789a1987 <47248675+56789a1987@users.noreply.github.com>
2024-05-13 10:19:28 +0800

    Updated build scripts

--- make.bat
+++ make.bat
@@ -1,17 +1,13 @@
 REM Fix this path !!!
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
-set v93=v16.20.0
-set v108=v18.18.0
-set v115=v20.9.0
-set v120=v21.1.0
+set v108=v18.20.0
+set v115=v20.12.0
+set v120=v21.7.0
+set v127=v22.1.0
 
 if not exist targets (
   mkdir targets
-
-  wget -c https://nodejs.org/dist/%v93%/node-%v93%-headers.tar.gz -P targets
-  tar -xzf targets/node-%v93%-headers.tar.gz -C targets
-  wget https://nodejs.org/dist/%v93%/win-x64/node.lib -O targets/node-%v93%/node.lib
 
   wget -c https://nodejs.org/dist/%v108%/node-%v108%-headers.tar.gz -P targets
   tar -xzf targets/node-%v108%-headers.tar.gz -C targets
@@ -24,12 +20,16 @@ if not exist targets (
   wget -c https://nodejs.org/dist/%v120%/node-%v120%-headers.tar.gz -P targets
   tar -xzf targets/node-%v120%-headers.tar.gz -C targets
   wget https://nodejs.org/dist/%v120%/win-x64/node.lib -O targets/node-%v120%/node.lib
+
+  wget -c https://nodejs.org/dist/%v127%/node-%v127%-headers.tar.gz -P targets
+  tar -xzf targets/node-%v127%-headers.tar.gz -C targets
+  wget https://nodejs.org/dist/%v127%/win-x64/node.lib -O targets/node-%v127%/node.lib
 )
 
-cl /std:c++17 /I src/headers/16 /I targets/node-%v93%/include/node /I targets/node-%v93%/deps/uv/include /I targets/node-%v93%/deps/v8/include /I targets/node-%v93%/deps/openssl/openssl/include /I targets/node-%v93%/deps/zlib /EHsc /Ox /LD /Fedist/bindings/cws_win32_93.node src/*.cpp targets/node-%v93%/node.lib
 cl /std:c++17 /I src/headers/18 /I targets/node-%v108%/include/node /I targets/node-%v108%/deps/uv/include /I targets/node-%v108%/deps/v8/include /I targets/node-%v108%/deps/openssl/openssl/include /I targets/node-%v108%/deps/zlib /EHsc /Ox /LD /Fedist/bindings/cws_win32_108.node src/*.cpp targets/node-%v108%/node.lib
 cl /std:c++17 /I src/headers/20 /I targets/node-%v115%/include/node /I targets/node-%v115%/deps/uv/include /I targets/node-%v115%/deps/v8/include /I targets/node-%v115%/deps/openssl/openssl/include /I targets/node-%v115%/deps/zlib /EHsc /Ox /LD /Fedist/bindings/cws_win32_115.node src/*.cpp targets/node-%v115%/node.lib
 cl /std:c++17 /I src/headers/21 /I targets/node-%v120%/include/node /I targets/node-%v120%/deps/uv/include /I targets/node-%v120%/deps/v8/include /I targets/node-%v120%/deps/openssl/openssl/include /I targets/node-%v120%/deps/zlib /EHsc /Ox /LD /Fedist/bindings/cws_win32_120.node src/*.cpp targets/node-%v120%/node.lib
+cl /std:c++17 /I src/headers/22 /I targets/node-%v127%/include/node /I targets/node-%v127%/deps/uv/include /I targets/node-%v127%/deps/v8/include /I targets/node-%v127%/deps/openssl/openssl/include /I targets/node-%v127%/deps/zlib /EHsc /Ox /LD /Fedist/bindings/cws_win32_127.node src/*.cpp targets/node-%v127%/node.lib
 
 del ".\*.obj"
 del ".\dist\bindings\*.exp"
