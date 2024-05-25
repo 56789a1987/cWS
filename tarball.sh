e9b3a2824ee44716ddf54dfb43e5af3dbeecd475
56789a1987 <47248675+56789a1987@users.noreply.github.com>
2024-05-25 16:58:03 +0800

    Support downloading prebuilt binaries from GitHub

--- tarball.sh
+++ tarball.sh
@@ -1,8 +1,7 @@
 PLATFORM=$(node -p "['node-v' + process.versions.modules, process.platform, process.arch].join('-')")
 TARNAME=cws-$BUILD_TAG-$PLATFORM.tar.gz
-FILES=$(cd dist/bindings && echo *.node)
 
-tar -C dist/bindings -czvf "$TARNAME" $FILES
+tar -czvf "$TARNAME" dist/bindings/*.node
 
 if [ $? -ne 0 ]; then
   echo "failed to make tarball $TARNAME"
